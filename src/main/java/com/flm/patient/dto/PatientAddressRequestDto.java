4926605014f53dfd58f49069e1924752896513e3
KarnatiAlekhya <alekhyakarnati5759@gmail.com>
2026-09-05 20:34:36 +0530

    Create RegisterPatientRequestDto

---
+++ src/main/java/com/flm/patient/dto/PatientAddressRequestDto.java
@@ -0,0 +1,12 @@
+package com.flm.patient.dto;
+
+
+public class PatientAddressRequestDto {
+
+    private String doorNumber;
+    private String landmark;
+    private String city;
+    private String state;
+    private String country;
+    private String pinCode;
+}
