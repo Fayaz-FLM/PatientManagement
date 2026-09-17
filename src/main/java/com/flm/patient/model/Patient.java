bb5eda9c75a2accbb0fa62d72570cc75e6fbef35
harshareddy-thotli <harshareddy2038@gmail.com>
2026-09-17 15:15:04 +0530

    Change import from Gender enum to Gender utils

--- src/main/java/com/flm/patient/model/Patient.java
+++ src/main/java/com/flm/patient/model/Patient.java
@@ -1,7 +1,7 @@
 package com.flm.patient.model;
 
 import java.time.LocalDate;
-import com.flm.patient.enum.Gender;
+import com.flm.patient.utils.Gender;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
